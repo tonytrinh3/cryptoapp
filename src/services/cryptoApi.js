9e9c32b22a51e6121b20164cfc19a00bbfd02db9
Tony Trinh <tony.w.trinh3@gmail.com>
2022-04-06 22:34:11 -0400

    started writing cryptocurrency component and made api not available public

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,29 +2,10 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders ={
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '48961dddddmsh6a00af64edb9004p1a3176jsnbc7ce3b583ed'
+    'X-RapidAPI-Key': process.env.REACT_APP_COINRANKING_API_KEY
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com'
-
-
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coins',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       timePeriod: '24h',
-//       tiers: '1',
-//       orderBy: 'marketCap',
-//       orderDirection: 'desc',
-//       limit: '50',
-//       offset: '0'
-//     },
-//     headers: {
-//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-//       'X-RapidAPI-Key': '48961dddddmsh6a00af64edb9004p1a3176jsnbc7ce3b583ed'
-//     }
-//   };
 
 const createRequest = (url)=>({url,headers: cryptoApiHeaders})
 
