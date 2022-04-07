1e4b8676710b84e02ff40cfc144678c0a915b20c
Tony Trinh <tony.w.trinh3@gmail.com>
2022-04-06 21:00:07 -0400

    finished outline

--- src/component/Homepage.jsx
+++ src/component/Homepage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import {Link} from 'react-router-dom'
+
+const {Title} = Typography;
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            
+        </>
     )
 }
 
