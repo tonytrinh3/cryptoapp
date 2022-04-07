fd6da1f450e45e2e002d5e8a2a768b365ff6dcf9
Tony Trinh <tony.w.trinh3@gmail.com>
2022-04-06 22:07:44 -0400

    able to render fetched coin data from api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,4 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders ={
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
@@ -10,13 +10,15 @@ const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
 // const options = {
 //     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
 //     params: {
 //       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       tiers: '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
 //       limit: '50',
-//       offset: '0',
-//       orderBy: '24hVolume',
-//       orderDirection: 'desc'
+//       offset: '0'
 //     },
 //     headers: {
 //       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
@@ -31,7 +33,11 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({baseUrl}),
     endpoints: (builder)=>({
         getCryptos: builder.query({
-            query:()=>createRequest('/exchanges')
+            query:()=>createRequest('/coins')
         })
     })
 })
+
+export const {
+    useGetCryptosQuery //it is a hook that is made from redux to allow you to get the data of the crypto api
+} = cryptoApi;
