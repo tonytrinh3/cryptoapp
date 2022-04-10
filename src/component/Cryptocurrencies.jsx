ff495087a060fb5d94087d9822e66eed3d465dc4
Tony Trinh <tony.w.trinh3@gmail.com>
2022-04-10 13:33:16 -0400

    finished cryptocurrency component

--- src/component/Cryptocurrencies.jsx
+++ src/component/Cryptocurrencies.jsx
@@ -1,23 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = ({simplified}) => {
+const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100;
     const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+    const [cryptos, setCryptos] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        //setCryptos(cryptosList?.data?.coins)
+        const filteredData = cryptosList?.data?.coins.filter((coin) =>
+            coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setCryptos(filteredData); // this will still render the full list bc searchTerm is empty on render`
+    }, [cryptos, searchTerm]);
 
     if (isFetching) return "Loading ..."; // apparently this is important when fetching api to make sure if the api is fetched...
 
     return (
         <>
-            <div className="search-crypto">
-                <Input placeholder="Search Cryptocurrency" onChange={(e)=>{setSearchTerm(e.target.value)}}></Input>
-            </div>
+            {/* {!simplified ? (
+                <div className="search-crypto">
+                    <Input
+                        placeholder="Search Cryptocurrency"
+                        onChange={(e) => {
+                            setSearchTerm(e.target.value);
+                        }}
+                    ></Input>
+                </div>
+            ) : null} */}
+
+            {!simplified && (
+                <div className="search-crypto">
+                    <Input
+                        placeholder="Search Cryptocurrency"
+                        onChange={(e) => {
+                            setSearchTerm(e.target.value);
+                        }}
+                    ></Input>
+                </div>
+            )}
+
             <Row gutter={[32, 32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency?.uuid}>
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,25 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const cryptoNewsHeaders ={
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+    'X-RapidAPI-Key': REACT_APP_BING_NEWS_API_KEY
+  }
+
+  const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+  const createRequest = (url)=>({url, headers: cryptoNewsHeaders})
+
+  export const cryptoNewsApi = createApi({
+    reducerPath: "cryptoNewsApi",
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder)=>({
+        getCryptoNews: builder.query({
+            query:({newsCategory,count})=>createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+})
+
+export const {
+    useGetCryptoNewsQuery //it is a hook that is made from redux to allow you to get the data of the crypto api
+} = cryptoNewsApi;
